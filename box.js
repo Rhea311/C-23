dbfc0372b736383295af634b79dbcb5001cf4bd4
Rhea311 <76726836+Rhea311@users.noreply.github.com>
2021-01-15 13:10:13 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+class box
+{
+
+constructor(x,y,w,h){
+
+  var option = {
+    restitution : 1.0
+  }
+  this.body = Bodies.rectangle(x,y,w,h,option);
+  this.width = w;
+  this.height = h;
+  World.add(world,this.body);
+  
+}
+
+display(){
+   var pos = this.body.position
+
+
+  rectMode(CENTER);
+  rect(pos.x, pos.y,this.width,this.height)
+   
+
+}
+
+}
